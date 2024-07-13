643e00518fe1f7b01d9ccdb443fe54801026b135
David <davidmwathi412@gmail.com>
2024-07-13 12:16:15 +0300

    Added changes

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(correctanswer=4) {
+    // Function body
+};
+
+document.querySelector ("quiz");
+let value = document.querySelector ("quiz");
+let userAnswer = value
+
+let feedback = document.getElementById("feedback");
+if("userAnswer==correctAnswer"){
+   feedback="Correct!Well done"
+}else{
+    feedback="That's incorrect. Try again"
+}
+
+let SubmitAnswer= document.getElementById("submit-answer")
