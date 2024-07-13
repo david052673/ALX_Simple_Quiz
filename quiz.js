c5cf6ec869229dc8eb4de72d90729d18946ffde2
David <davidmwathi412@gmail.com>
2024-07-13 20:47:05 +0300

    Added changes

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ let userAnswer = value
 
 let feedback = document.getElementById("feedback");
 ["if (userAnswer === correctAnswer)"]{
-   feedback="Correct!Well done"
+   feedback=["Correct! Well done."]
 }["else"]{
     feedback="That's incorrect. Try again"
 }
